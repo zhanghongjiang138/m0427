74c6cbcbde3a0ae0c478625d64754e7349e15c7d
zhanghongjiang <zhanghongjiang@PC-00-002681-1.Geely.Auto>
2016-01-21 10:18:42 +0800

    1

--- web/src/main/java/Runner.java
+++ web/src/main/java/Runner.java
@@ -11,7 +11,7 @@ public class Runner implements Runnable{
   public void run() {
     while(true)
     {
-      System.out.println(runnerName+": "+i++);
+      System.out.println(runnerName+":: "+i++);
     }
   }
   
