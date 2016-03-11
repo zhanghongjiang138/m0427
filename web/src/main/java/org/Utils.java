5f7b81d1ae7cc6aedc9d1be2700b4ecc7b381d69
zhanghongjiang <zhanghongjiang@PC-00-002681-1.Geely.Auto>
2016-03-11 17:59:48 +0800

    jkit

--- web/src/main/java/org/Utils.java
+++ web/src/main/java/org/Utils.java
@@ -1,10 +1,13 @@
 package org;
 
+import java.util.Hashtable;
+
 public class Utils {
   
   public static void print(Object o )
   {
     System.out.print(o);
+    Hashtable t;
   }
   
   public static void println(Object o )
